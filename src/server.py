7064c776c0ae0723cd55984b57d9e7d877a926a8
Peter Kim <seattlechem@yahoo.com>
2018-04-03 22:41:19 -0700

    initial server.py

--- src/server.py
+++ src/server.py
@@ -1,7 +1,8 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
+from cowpy import cow
 import json
-import sys
+import io
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -9,18 +10,54 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         parsed_path = urlparse(self.path)
         parsed_qs = parse_qs(parsed_path.query)
 
-        # import pdb; pdb.set_trace()
-
+        # GET / - returns a valid HTML formatted response with a project \
+        # description and an anchor tag which references a link to /cow.
         if parsed_path.path == '/':
             self.send_response(200)
             self.end_headers()
 
-            self.wfile.write(b'You did a thing!')
+            self.wfile.write(b'''
+            <!DOCTYPE html>
+            <html>
+            <head>
+            <title> cowsay </title>
+            </head>
+            <body>
+            <header>
+            <nav>
+            <ul>
+            <li><a href="/cowsay">cowsay</a></li>
+            </ul>
+            </nav>
+            <header>
+            <main>
+            <!-- project description -->
+            </main>
+            </body>
+            </html>
+            ''')
+            return
+        # GET /cowsay - returns a generic cowpy response which displays a \
+        # helpful message to the client about how they can further interact \
+        # with the API.
+        elif parsed_path.path == '/cowsay':
+            cheese = cow.Moose()
+            msg = cheese.milk('yo im a moose')
+            print(msg)
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(msg.encode('utf8'))
             return
 
-        elif parsed_path.path == '/test':
+        # GET /cow?msg=text - returns a cowpy response which \
+        # correctly displays a default cow object including \
+        # the text from your query string.
+        elif parsed_path.path == '/cow':
             try:
-                cat = json.loads(parsed_qs['category'][0])
+                message = json.loads(parsed_qs['msg'][0])
+                cheese = cow.Moose()
+                msg = cheese.milk(message)
+                print(msg)
             except KeyError:
                 self.send_response(400)
                 self.end_headers()
@@ -37,10 +74,22 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'Not Found')
 
+    # POST /cow msg=text - returns a cowpy response with a JSON body \
+    # {"content": "<cowsay cow>"}
     def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
         self.send_response(200)
         self.end_headers()
-        self.send_response_only()
+        response = io.BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+        # self.send_response(200)
+        # self.end_headers()
+        # self.send_response_only()
 
 
 def create_server():
